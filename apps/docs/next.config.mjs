ed22f2b7e273858ea2f4b2675bbba5e812613a45
Sourabh Malviya <saurabhmalvia997@gmail.com>
2026-02-08 18:05:37 +0530

    refactor: update deploy workflow and next config

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -4,18 +4,17 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/moe-ui" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/moe-ui" : "",
+  // basePath: process.env.NODE_ENV === "production" ? "/moe-ui" : "",
+  // assetPrefix: process.env.NODE_ENV === "production" ? "/moe-ui" : "",
   reactStrictMode: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/docs/:path*.mdx',
-  //       destination: '/llms.mdx/docs/:path*',
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/docs/:path*.mdx',
+        destination: '/llms.mdx/docs/:path*',
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
